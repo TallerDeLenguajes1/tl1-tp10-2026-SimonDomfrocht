b474cd026d28aaef22b8e88a89128c684fa7d3d7
Simon Domfrocht <simonbussetti01@gmail.com>
2026-06-22 21:52:15 -0300

    Ejercicio 2

---
+++ Usuarios/Program.cs
@@ -0,0 +1,30 @@
+﻿using espacioUsuarios;
+using System.Text.Json;
+
+HttpClient cliente = new HttpClient();
+
+HttpResponseMessage respuesta = await cliente.GetAsync("https://jsonplaceholder.typicode.com/users"); 
+respuesta.EnsureSuccessStatusCode();
+
+string jsonCrudo = await respuesta.Content.ReadAsStringAsync(); 
+List<Root>? listaUsuarios = JsonSerializer.Deserialize<List<Root>>(jsonCrudo); //guardo el json y lo deserializo
+
+if (listaUsuarios == null)
+{
+    Console.WriteLine("No se pudieron cargar los usuarios.");
+    return;
+}
+
+
+Console.WriteLine("\nPRIMEROS 5 USUARIOS: \n");
+int contador = 0;
+foreach (var usuario in listaUsuarios)
+{   
+    
+    Console.WriteLine(" Id: " + usuario.id + " | Nombre: " + usuario.name + " | Correo: " + usuario.email + " | Domicilio: " + usuario.address.street);
+    contador++;
+    if (contador == 5)
+    {
+        break;
+    }
+}
